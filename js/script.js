705fa849640f7cec5efc5f8185b3cdf4e682b431
thanujsai <sboda@modeln.com>
2024-04-04 16:10:16 +0530

    display-popular-movies

--- js/script.js
+++ js/script.js
@@ -4,6 +4,54 @@ const global = {
 
 console.log(global.currentPage);
 
+async function displayPopularMovies(){
+    console.log('display popular movies')
+    const result= await fetchAPIData('movie/popular'); //output = {page: 1, results: Array(20), total_pages: 43380, total_results: 867583}
+    const { results } = await fetchAPIData('movie/popular'); //curly braces around results to get the array of results(movies) directly
+    console.log(results)
+    console.log(result)
+
+    results.forEach(movie => {
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `
+        <a href="movie-details.html?id=${movie.id}">
+          ${
+            movie.poster_path
+            ? `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top" alt="${movie.title}" />` : `
+            <img
+            src="images/no-image.jpg"
+            class="card-img-top"
+            alt="Movie Title"
+          />
+            `
+          }
+        </a>
+        <div class="card-body">
+          <h5 class="card-title">${movie.title}</h5>
+          <p class="card-text">
+            <small class="text-muted">Release: ${movie.release_date}</small>
+          </p>
+        </div>
+        `;
+
+    document.querySelector('#popular-movies').appendChild(div);
+    });
+}
+
+//fetch data from TMDB API
+async function fetchAPIData(endpoint) {
+    const API_KEY = '3e7aa45f43cc96777cf7126767f67911';
+    const API_URL = 'https://api.themoviedb.org/3/';
+
+    const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
+
+    const data = await response.json();
+
+    return data;
+
+}
+
 //highlight active link: top right movies or tv shows
 function highlightActiveLink() {
     const links = document.querySelectorAll('.nav-link');
@@ -30,6 +78,7 @@ function init() {
         case '/':
         case '/index.html':
             console.log('Home');
+            displayPopularMovies();
             break;
         case '/shows.html':
             console.log('Shows');
