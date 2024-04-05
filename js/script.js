11a496275827b447a85e39d736cdbcb54c6ff163
thanujsai <sboda@modeln.com>
2024-04-05 11:39:51 +0530

    now-playing

--- js/script.js
+++ js/script.js
@@ -223,6 +223,55 @@ function displayBackgroundImage(type, backgroundPath){
         document.querySelector('#show-details').appendChild(overlayDiv);
     }
 }
+
+//display slider movies
+async function displaySlider(){
+  const {results} = await fetchAPIData('movie/now_playing');
+  
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('swiper-slide');
+
+    div.innerHTML = `
+    <a href="movie-details.html?id=${movie.id}">
+      <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+    </a>
+    <h4 class="swiper-rating">
+      <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+    </h4>
+    `;
+
+    document.querySelector('.swiper-wrapper').appendChild(div);
+
+    initSwiper();
+  });
+
+}
+
+function initSwiper(){
+  const swiper = new Swiper('.swiper',{
+    slidesPerView : 1,
+    spaceBetween: 30,
+    freeMode: true,//we can drag the slider to move around
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false
+    },
+    breakpoints: {//resize as per the window size, if we change the window size of the browser number of movies shown in slider will be changed
+      500: {
+        slidesPerView: 2
+      },
+      700: {
+        slidesPerView: 3
+      },
+      1200: {
+        slidesPerView: 4
+      }
+    }
+  });
+}
+
 //fetch data from TMDB API
 async function fetchAPIData(endpoint) {
     const API_KEY = '3e7aa45f43cc96777cf7126767f67911';
@@ -273,6 +322,7 @@ function init() {
         case '/index.html':
             console.log('Home');
             displayPopularMovies();
+            displaySlider();
             break;
         case '/shows.html':
             console.log('Shows');
